b1557e8fdd7b4d3608f4eb517d4ca41dc01263cb
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2025-07-08 00:41:03 +0530

    Minior bug fix

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./options";
-
-const { handlers } = NextAuth(authOptions);
-
-export const GET = handlers.GET;
-export const POST = handlers.POST;
